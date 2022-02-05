26fb9b7f7832b5c3b8ff7d88cb8140755e8c2427
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-02-05 21:56:17 +0900

    nomad코딩1

---
+++ nomade.js
@@ -0,0 +1,13 @@
+const question = prompt("How old are You?");
+
+const age = "8"
+// typeof 타입 확인 parseInt 형변환
+console.log(typeof question, parseInt(age));
+// isNaN boolean 값을 반환 
+console.log(isNaN(age));
+// parseInt(age) age에 들어가 있는 값이 숫자가 아닌 경우 NaN반환
+if(condition){
+
+}else{
+
+}
