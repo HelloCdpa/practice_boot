de68146a797dfbca25249602b667b1e849f01e0d
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-02-10 20:45:18 +0900

    background

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const heart = document.querySelector("#like")
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
 }
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+
+
+function handleClick(){
+    heart.src = "/img/like-click.png"
+}
+heart.addEventListener("click",handleClick);
+
+
+function handleCopy(){
+    heart.src = "/img/like_empty.png"
+}
+window.addEventListener("copy",handleCopy);
